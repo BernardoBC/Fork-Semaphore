ba336b0f557836213a0f9c8b944fc7de6b3ba356
Bernardo <bernardobonillac@gmail.com>
2014-07-05 18:38:52 -0600

    shmkey and shmid 0 to 1, 2 to 1

--- test.c
+++ test.c
@@ -28,11 +28,11 @@ int main(void)
 	int numeroHijos = 0;
 
 	/*Memoria compartida*/
+	key_t shmkey_0;                 /*      shared memory key       */
 	key_t shmkey_1;                 /*      shared memory key       */
-	key_t shmkey_2;                 /*      shared memory key       */
 	key_t shmkey_3;                 /*      shared memory key       */
-	int shmid_1;                    /*      shared memory id        */	
-	int shmid_2;                    /*      shared memory id        */
+	int shmid_0;                    /*      shared memory id        */	
+	int shmid_1;                    /*      shared memory id        */
 	int shmid_3;                    /*      shared memory id        */
 
 	sem_t *sem;                   /*      synch semaphore         *//*shared */
@@ -40,39 +40,27 @@ int main(void)
 	int *p;                       /*      shared variable         *//*shared */
 
 	/*Agregar valor isPlayerCreated a memoria compartida*/
-    shmkey_1 = ftok ("/dev/null", 5);       /* valid directory name and a number */
-    printf ("shmkey for isPlayersCreated = %d\n", shmkey_1);
-    shmid_1 = shmget (shmkey_1, sizeof (int), 0644 | IPC_CREAT);
-    if (shmid_1 < 0){                           /* shared memory error check */
+    shmkey_0 = ftok ("/dev/null", 5);       /* valid directory name and a number */
+    printf ("shmkey for isPlayersCreated = %d\n", shmkey_0);
+    shmid_0 = shmget (shmkey_0, sizeof (int), 0644 | IPC_CREAT);
+    if (shmid_0 < 0){                           /* shared memory error check */
         perror ("shmget\n");
         exit (1);
     }
-    isPlayersCreated = (int *) shmat (shmid_1, NULL, 0);
+    isPlayersCreated = (int *) shmat (shmid_0, NULL, 0);
     *isPlayersCreated = 0;
     printf ("isPlayersCreated=%d is allocated in shared memory.\n\n", *isPlayersCreated);
 
 
     /*Marco 1 recurso compartido*/
-    shmkey_2 = ftok ("/dev/null", 6);       /* valid directory name and a number */
-    printf ("shmkey for marco1 = %d\n", shmkey_2);
-    shmid_2 = shmget (shmkey_2, sizeof (int), 0644 | IPC_CREAT);
-    if (shmid_2 < 0){                           /* shared memory error check */
+    shmkey_1 = ftok ("/dev/null", 6);       /* valid directory name and a number */
+    printf ("shmkey for marco1 = %d\n", shmkey_1);
+    shmid_1 = shmget (shmkey_1, sizeof (int), 0644 | IPC_CREAT);
+    if (shmid_1 < 0){                           /* shared memory error check */
         perror ("shmget\n");
         exit (1);
     }
-    marco1 = (int *) shmat (shmid_2, NULL, 0);
-    *marco1 = 0;
-    printf ("marco1=%d is allocated in shared memory.\n\n", *marco1);
-
-    /*Marco 2 recurso compartido*/
-    shmkey_2 = ftok ("/dev/null", 6);       /* valid directory name and a number */
-    printf ("shmkey for marco1 = %d\n", shmkey_2);
-    shmid_2 = shmget (shmkey_2, sizeof (int), 0644 | IPC_CREAT);
-    if (shmid_2 < 0){                           /* shared memory error check */
-        perror ("shmget\n");
-        exit (1);
-    }
-    marco1 = (int *) shmat (shmid_2, NULL, 0);
+    marco1 = (int *) shmat (shmid_1, NULL, 0);
     *marco1 = 0;
     printf ("marco1=%d is allocated in shared memory.\n\n", *marco1);
 
@@ -134,8 +122,8 @@ int main(void)
 		printf("numero de Hijos %d, isPlayersCreated %d\n",numeroHijos, *isPlayersCreated);
 		/* shared memory detach */
         shmdt (isPlayersCreated);
+        shmctl (shmid_0, IPC_RMID, 0);
         shmctl (shmid_1, IPC_RMID, 0);
-        shmctl (shmid_2, IPC_RMID, 0);
 
         /* cleanup semaphores */
         sem_destroy (sem);
